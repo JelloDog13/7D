f95229dabaa04c27e700e7ab5cf150946bd114d0
Jello <lu.emmanuel@laposte.net>
2020-12-09 17:27:11 +0100

    Finition du GO son ambiance / musique

--- Assets/Scripts/AmbianceMusic.cs
+++ Assets/Scripts/AmbianceMusic.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class AmbianceMusic : MonoBehaviour
 {
-    [SerializeField] AudioClip _ambiance1, _ambiance2, _ambiance3;
-    [SerializeField] AudioSource _audioAmbi, _audioMusic;
+    [Header("Ambiance Clips")]
+    [SerializeField] AudioClip _ambiance1;
+    [SerializeField] AudioClip _ambiance2;
+    [SerializeField] AudioClip _ambiance3;
+    [Header("Music Clips")]
+    [SerializeField] AudioClip _music1;
+    [SerializeField] AudioClip _music2;
+    [SerializeField] AudioClip _music3;
 
+    [Header("Components")]
+    [SerializeField] AudioSource _audioAmbi, _audioMusic;
 
     void Start()
     {
@@ -17,10 +25,12 @@ public class AmbianceMusic : MonoBehaviour
     {
         _audioAmbi.clip = _ambiance1;
     }
+
     public void SwitchTo2()
     {
         _audioAmbi.clip = _ambiance2;
     }
+
     public void SwitchTo3()
     {
         _audioAmbi.clip = _ambiance3;
