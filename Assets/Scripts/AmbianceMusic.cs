9cbb3c13038582e674970b607233e1be7a11e4b3
Jello <lu.emmanuel@laposte.net>
2020-12-09 16:05:19 +0100

    Ajout du GO ambiance son

---
+++ Assets/Scripts/AmbianceMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbianceMusic : MonoBehaviour
+{
+    [SerializeField] AudioClip _ambiance1, _ambiance2, _ambiance3;
+    private AudioSource _audio;
+
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
