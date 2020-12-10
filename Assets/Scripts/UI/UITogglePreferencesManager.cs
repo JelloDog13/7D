f11e07573e0966b1f38315222f289ee6e3bd16c8
DspSola <robin.sola@gmail.com>
2020-12-10 17:23:25 +0100

    Prparatio scripts UI

--- Assets/Scripts/UI/UITogglePreferencesManager.cs
+++ Assets/Scripts/UI/UITogglePreferencesManager.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class UITogglePreferencesManager : MonoBehaviour
+[RequireComponent(typeof(Toggle))]
+public class UITogglePreferencesManager : UIPreferencesManager
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool _defaultValue;
+
+    private void Awake()
     {
-        
+        _toggle = GetComponent<Toggle>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void LoadPrefs()
     {
-        
+        if(PlayerPrefs.HasKey(_settingName))
+        {
+            int value = PlayerPrefs.GetInt(_settingName);
+            Toggle.isOn = value != 0;
+        }
+        else
+        {
+            Toggle.isOn = _defaultValue;
+        }
+    }
+
+    public override void SavePref()
+    {
+        PlayerPrefs.SetInt(_settingName, (Toggle.isOn) ? 1 : 0);
+    }
+
+    private Toggle _toggle;
+
+    private Toggle Toggle
+    {
+        get
+        {
+            if (_toggle == null)
+            {
+                _toggle = GetComponent<Toggle>();
+            }
+            return _toggle;
+        }
     }
 }
