e5d89fe6aafc7a5b845ccfddcb316edaa7b87189
Victor DS <vsenneville@yahoo.fr>
2020-12-11 18:32:53 +0100

    Début de cutscene a l'interaction

---
+++ Assets/QuestItemInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestItemInteraction : Interaction
+{
+    [SerializeField] TMP_Text _pickUpItemText;
+    private void InteractWithQuestObject(string tag)
+    {
+        base.InteractWithObject(tag);
+        _pickUpItemText.text = "Press V to pick up" + " " + "the" + " " + tag;
+    }
+
+    private void Update()
+    {
+        InteractWithQuestObject("Flower");
+        InteractWithQuestObject("DogToy");
+        InteractWithQuestObject("Logs");
+    }
+}
--- Assets/Victor Test/Interaction.cs
+++ Assets/Victor Test/Interaction.cs
@@ -2,23 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using FPSControllerLPFP;
 
-public class Interaction : MonoBehaviour
+public abstract class Interaction : MonoBehaviour
 {
     [SerializeField] Rigidbody _playerRb;
     [SerializeField] ItemChecker _itemChecker;
-    [SerializeField] TMP_Text _pickUpItemText;
+    [SerializeField] Camera _mainCam;
+    [SerializeField] float _cutSceneTime;
+    [SerializeField] GameObject _player;
+
+    //    public virtual void InteractWithObject(string tag)
+    //    {
+    //        if (_itemChecker.ItemCheck() && _itemChecker._hitBuffer[0].collider.CompareTag(tag))
+    //        {
+    //            _pickUpItemText.text = "Press V to pick up" + " " + "the" + " " + tag;
+
+    //            if (Input.GetKeyDown(KeyCode.V))
+    //            {
+    //                _playerRb.constraints = RigidbodyConstraints.FreezeAll;
+
+    //                if (_itemChecker._hitBuffer[0].collider.CompareTag("Flower"))
+    //                {
+    //                    _itemChecker._hitBuffer[0].collider.gameObject.SetActive(false);
+    //                }
+
+    //                if (_itemChecker._hitBuffer[0].collider.CompareTag("DogToy"))
+    //                {
+    //                    _itemChecker._hitBuffer[0].collider.gameObject.SetActive(false);
+    //                }
+
+    //                if (_itemChecker._hitBuffer[0].collider.CompareTag("Logs"))
+    //                {
+    //                    _itemChecker._hitBuffer[0].collider.gameObject.SetActive(false);
+    //                }
+    //            }
+    //        }
+    //    }
+
     public virtual void InteractWithObject(string tag)
     {
-        if (_itemChecker.ItemCheck() && _itemChecker._hitBuffer[0].collider.CompareTag(tag))
+        if (_itemChecker.ItemCheck() /*&& _itemChecker._hitBuffer[0].collider.CompareTag(tag)*/)
         {
-            _pickUpItemText.text = "Press V to pick up" + " " + "the" + " " + tag;
-
             if (Input.GetKeyDown(KeyCode.V))
             {
-                _playerRb.constraints = RigidbodyConstraints.FreezeAll;
-                _itemChecker._hitBuffer[0].collider.gameObject.SetActive(false);
+                Debug.Log("Coroutine Start");
+                StartCoroutine(CutSceneCoroutine());
             }
         }
+    }
+
+    IEnumerator CutSceneCoroutine()
+    {
+        _playerRb.isKinematic = true;
+        _player.GetComponent<FpsControllerLPFP>().enabled = false;
+        //_mainCam.transform.LookAt(_itemChecker._hitBuffer[0].transform);
+        //FONDU AU NOIR
+        yield return new WaitForSeconds(_cutSceneTime);
+        _playerRb.isKinematic = false;
+        _player.GetComponent<FpsControllerLPFP>().enabled = true;
     }
 }
