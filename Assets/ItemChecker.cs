920d7676b05edd05890a96913d2716e818fc7693
Jello <lu.emmanuel@laposte.net>
2020-12-13 02:04:42 +0100

    Scripts

---
+++ Assets/ItemChecker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemChecker : MonoBehaviour
+{
+    #region Show In Inspector
+
+    [Header("GUI")]
+    [SerializeField] Color _guizmoColor;
+
+    [Header("RayTracing")]
+    [SerializeField] float _checkDistance;
+    [SerializeField] LayerMask _isItAnInteractableObject;
+    [SerializeField] Transform _rayStartPos;
+    [SerializeField] public RaycastHit[] _hitBuffer = new RaycastHit[1];
+    public Collectible _targetObject;
+
+    #endregion
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _guizmoColor;
+        Vector3 direction = transform.TransformDirection(Vector3.forward) * _checkDistance;
+        Gizmos.DrawRay(_rayStartPos.position, direction);
+    }
+
+    public bool ItemCheck()
+    {
+        int rayHitCount = Physics.RaycastNonAlloc(_rayStartPos.position, transform.TransformDirection(Vector3.forward), _hitBuffer, _checkDistance, _isItAnInteractableObject);
+        
+        return rayHitCount > 0;
+    }
+
+    public string RaycastTag()
+    {
+        for (int i = 0; i < _hitBuffer.Length; i++)
+        { 
+            _hitBuffer[i].transform.TryGetComponent<Collectible>(out _targetObject);
+            return _hitBuffer[i].transform.tag;
+        }
+        return null;
+    }
+
+}
---
+++ Assets/QuestZone.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestZone : MonoBehaviour
+{
+    [SerializeField] int _questNumber;
+    [SerializeField] PlayerProgress _player;
+    [SerializeField] GameObject _targetObject;
+    private bool _questDone;
+    private bool _textAvailable = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_player.HasItem(_questNumber) && !_questDone)
+        {
+            Debug.Log("Quest DONE !");
+            _player.RemoveItem(_questNumber);
+            if (_player.SanityLevel() != 3)
+            {
+                _targetObject.SetActive(true);
+            }
+            //SON DE QUETE ACCOMPLIE
+        }
+        else ShowQuestLine();
+        Debug.Log("Get the item !");
+    }
+
+    private void ShowQuestLine()
+    {
+        if (_textAvailable)
+        {
+            StartCoroutine(TextCooldown());
+            //TEXTE BADASS
+        }
+    }
+
+    private IEnumerator TextCooldown()
+    {
+        _textAvailable = false;
+        yield return new WaitForSeconds(5);
+        _textAvailable = true;
+    }
+
+}
