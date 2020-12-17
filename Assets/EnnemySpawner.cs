d924bd66616d066d955000da4c52701322d70734
Jello <lu.emmanuel@laposte.net>
2020-12-17 15:34:06 +0100

    Polish ma biche

--- Assets/EnnemySpawner.cs
+++ Assets/EnnemySpawner.cs
@@ -16,7 +16,7 @@ public class EnnemySpawner : MonoBehaviour
         {
             for (int i = 0; i < numberOfEnnemies; i++)
             {
-                var obj = Instantiate(_ennemyPrefab, spawnPoint.position, Quaternion.identity, spawnPoint);
+                var obj = Instantiate(_ennemyPrefab, spawnPoint.position, Quaternion.identity);//, spawnPoint);
                 obj.transform.parent = GameObject.Find("BADDIES").transform;
             }
         }
